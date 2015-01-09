6487b7316189daca6cdfe22d7291029641b44182
Nikita Harlov <nikita@harlov.com>
2015-01-09 12:09:39 +0500

    call rpc model from web_main

--- src/eventflowng/web_main/views.py
+++ src/eventflowng/web_main/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from eventflowng.decorators.render_response import render_response
-
-
+from profitplatform_connector.ProfitPlatformRequest import ProfitPlatformRequest
+from profitplatform_connector.models import PlatformCommonDao
 def index(request):
-	data = {}
-	return render_response(request, 'webmain_index.html', data)
+    data = dict()
+    platform_common_dao = PlatformCommonDao()
+    
+    data['session'] = platform_common_dao.get_session()
+
+    return render_response(request, 'webmain_index.html', data)
